f614099b2f4b9aee5e371b370105f93f91038d24
Cizco-2005 <juanpablomoouc@gmail.com>
2026-09-21 21:05:04 -0600

    feat: completa practica 04

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,14 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:proyecto_integradormovil_equipo_4/main.dart';
 
 void main() {
-  testWidgets('La pantalla de registro se muestra correctamente', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const App());
-
-    expect(find.text('Registro de estudiantes'), findsOneWidget);
-    expect(find.text('Nombre'), findsOneWidget);
-    expect(find.text('Correo'), findsOneWidget);
-    expect(find.text('Agregar'), findsOneWidget);
+  testWidgets('Carga la pantalla inicial correctamente', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('Catálogo de Materias'), findsOneWidget);
   });
 }
